459690b327fce051cb27b659708ffe55e8b4be63
melwin911 <idkwhattoput30@gmail.com>
2025-07-30 14:32:44 +0800

    switched assets filepath

---
+++ src/assets/index.js
@@ -0,0 +1,80 @@
+import logo from "./logo.svg";
+import backend from "./backend.png";
+import creator from "./creator.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+
+import css from "./tech/css.png";
+import minitab from "./tech/minitab.png";
+import kotlin from "./tech/kotlin.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import reactjs from "./tech/reactjs.png";
+import adobe from "./tech/adobecreativecloud.png";
+import android from "./tech/android.png";
+import java from "./tech/java.png";
+import mysql from "./tech/mysql.png";
+import php from "./tech/php.png";
+import python from "./tech/python.png";
+import tableau from "./tech/tableau.png";
+import c from "./tech/c.png";
+
+import sp from "./company/sp.jpg";
+import ocbc from "./company/ocbc.png";
+import saf from "./company/saf.png";
+import sit from "./company/sit.png";
+
+import oop from "./oop.png";
+import websys from "./5hotel.png";
+import progfund from "./progfund.png";
+import dsa from "./dsa.png";
+import polyfyp from "./polyfyp.png";
+import oracle from "./oracle.png";
+import sd from "./sd.png";
+import mad from "./mad.png";
+import hackrift from "./hackrift.png";
+import ml from "./ml.png";
+import workato from "./workato.png";
+
+export {
+  logo,
+  backend,
+  creator,
+  mobile,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  kotlin,
+  minitab,
+  html,
+  javascript,
+  c,
+  reactjs,
+  sp,
+  ocbc,
+  saf,
+  sit,
+  oop,
+  websys,
+  sd,
+  progfund,
+  dsa,
+  mad,
+  hackrift,
+  ml,
+  polyfyp,
+  adobe,
+  android,
+  java,
+  mysql,
+  php,
+  python,
+  tableau,
+  oracle,
+  workato
+};
